4de3d0ae4955917d8908dd1720e607d037fd77ff
AnastasiaIQA12 <Sinka8900@mail.ru>
2021-02-16 23:04:46 +0400

    Change DataHelper and Test

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -12,11 +12,18 @@ public class DataHelper {
     private DataHelper() {
     }
 
-    public static AuthInfo getAuthInfo() throws SQLException {
+    @Value
+    public static class AuthInfo {
+        private String id;
+        private String login;
+        private String password;
+    }
+
+    public static AuthInfo getAuthInfo() {
         val idSQL = "SELECT id FROM users";
         val loginSQL = "SELECT login FROM users";
         val runner = new QueryRunner();
-        String login, id, password = "";
+        String login = "", id = "", password = "";
         try (
                 val conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "app", "pass"
                 );
@@ -24,6 +31,8 @@ public class DataHelper {
             id = (String) (runner.query(conn, idSQL, new ScalarHandler<>()));
             login = (String) (runner.query(conn, loginSQL, new ScalarHandler<>()));
 
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         if (login.equals("vasya")) {
             password = "qwerty123";
@@ -34,30 +43,41 @@ public class DataHelper {
         return new AuthInfo(id, login, password);
     }
 
-    public static VerificationCode getVerificationCode(AuthInfo authInfo) throws SQLException {
-        val codeSQL = "SELECT code FROM auth_codes WHERE created = (SELECT max(created) FROM auth_codes);";
+    @Value
+    public static class VerificationCode {
+        private String code;
+    }
+
+    public static VerificationCode getVerificationCode(AuthInfo authInfo) {
+        val codeSQL = "SELECT code FROM auth_codes WHERE created = (SELECT max(created) FROM auth_codes WHERE user_id='" + authInfo.id + "')";
         val runner = new QueryRunner();
-        String verificationCode;
+        String verificationCode = "";
         try (
                 val conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "app", "pass"
                 );
         ) {
-            val code = runner.query(conn, codeSQL, new ScalarHandler<>());
-            verificationCode = (String) code;
+            verificationCode = (String) runner.query(conn, codeSQL, new ScalarHandler<>());
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return new VerificationCode(verificationCode);
     }
 
-    @Value
-    public static class AuthInfo {
-        private String id;
-        private String login;
-        private String password;
-    }
-
-    @Value
-    public static class VerificationCode {
-        private String code;
+    public static void clearData() {
+        val deleteUserSQL = "DELETE FROM users";
+        val deleteCardSQL = "DELETE FROM cards";
+        val deleteAuthCodesSQL = "DELETE FROM auth_codes";
+        val runner = new QueryRunner();
+        try (
+                val conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "app", "pass"
+                );
+        ) {
+            runner.update(conn, deleteAuthCodesSQL);
+            runner.update(conn, deleteCardSQL);
+            runner.update(conn, deleteUserSQL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/test/java/ru/netology/test/AutorizationTest.java
+++ src/test/java/ru/netology/test/AutorizationTest.java
@@ -1,7 +1,6 @@
 package ru.netology.test;
 
 import com.codeborne.selenide.Condition;
-import org.apache.commons.dbutils.QueryRunner;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,15 +8,11 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selenide.open;
 
 import lombok.val;
-import org.openqa.selenium.Keys;
 import ru.netology.data.DataHelper;
 import ru.netology.page.LoginPage;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
-
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class AutorizationTest {
     @BeforeEach
@@ -26,23 +21,12 @@ public class AutorizationTest {
     }
 
     @AfterAll
-    static void tearDown() throws SQLException {
-        val deleteUserSQL = "DELETE FROM users";
-        val deleteCardSQL = "DELETE FROM cards";
-        val deleteAuthCodesSQL = "DELETE FROM auth_codes";
-        val runner = new QueryRunner();
-        try (
-                val conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "app", "pass"
-                );
-        ) {
-            runner.update(conn,deleteAuthCodesSQL);
-            runner.update(conn,deleteCardSQL);
-            runner.update(conn, deleteUserSQL);
-        }
+    static void tearDown() {
+        DataHelper.clearData();
     }
 
     @Test
-    void shouldAutorizationInPersonalArea() throws SQLException {
+    void shouldAuthorizationInPersonalArea() {
         val loginPage = new LoginPage();
         val authInfo = DataHelper.getAuthInfo();
         val verificationPage = loginPage.validLogin(authInfo);
@@ -52,18 +36,11 @@ public class AutorizationTest {
     }
 
     @Test
-    void shouldAutorizationInPersonalAreaWrongCode() throws SQLException {
+    void shouldAuthorizationInPersonalAreaWrongCode() {
         val loginPage = new LoginPage();
         val authInfo = DataHelper.getAuthInfo();
         val verificationPage = loginPage.validLogin(authInfo);
-        $("[data-test-id=code] input").setValue("12345");
-        $("[data-test-id=action-verify]").click();
-        $("[data-test-id=code] input").sendKeys(Keys.CONTROL + "A" + Keys.DELETE);
-        $("[data-test-id=code] input").setValue("54321");
-        $("[data-test-id=action-verify]").click();
-        $("[data-test-id=code] input").sendKeys(Keys.CONTROL + "A" + Keys.DELETE);
-        $("[data-test-id=code] input").setValue("67890");
-        $("[data-test-id=action-verify]").click();
+        verificationPage.wrongVerify("12345");
         $(withText("Система заблокирована")).shouldBe(Condition.visible);
     }
 }
